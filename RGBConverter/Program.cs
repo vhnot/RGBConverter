f38badb81b521c4bb5079d2f9f7f983c45f663dc
Volodymyr Hnot <vhnot@softserveinc.com>
2021-11-04 14:45:35 +0200

    Add project files.

---
+++ RGBConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RGBConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // trykcloud image format
+            string imagePathFormat = "https://scrl.img.trykcloudstatic.com/designs/{0}/colors/{1}/thumbnail?pixels={2}";
+
+            string styleNumber = "C013W";
+            string colorNumber = "05225";
+            int size = 20;
+
+            string imagePath = string.Format(imagePathFormat, styleNumber, colorNumber, size);
+            var color = RGBConverter.GetColor(imagePath);
+
+            Console.WriteLine($"R: {color.R} G: {color.G} B: {color.B}");
+        }
+    }
+}
---
+++ RGBConverter/RGBConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+
+namespace RGBConverter
+{
+    public static class RGBConverter
+    {
+        public static Color GetColor(string imagePath)
+        {
+            var image = GetBitmap(imagePath);
+
+            return GetColor(image);
+        }
+
+        private static Bitmap GetBitmap(string imagePath)
+        {
+            var request = WebRequest.Create(imagePath);
+            var response = request.GetResponse();
+            var stream = response.GetResponseStream();
+
+            return new Bitmap(stream);
+        }
+
+        private static Color GetColor(Bitmap bitmap)
+        {
+            int width = bitmap.Width,
+                height = bitmap.Height,
+                rSum = 0,
+                gSum = 0,
+                bSum = 0,
+                pixelsCount = width * height;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var color = bitmap.GetPixel(x, y);
+
+                    rSum += color.R;
+                    gSum += color.G;
+                    bSum += color.B;
+                }
+            }
+
+            int r = GetAvaragePixelValue(rSum, pixelsCount);
+            int g = GetAvaragePixelValue(gSum, pixelsCount);
+            int b = GetAvaragePixelValue(bSum, pixelsCount);
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private static byte GetAvaragePixelValue(int sum, int pixelsCount)
+        {
+            return (byte)Math.Round((double)sum / pixelsCount);
+        }
+    }
+}
